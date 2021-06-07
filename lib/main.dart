6afa08388eb225e51259fd29b5bdedcefaca04c9
Ahmad Fajrul Falah <fajar.falachudin@gmail.com>
2021-06-07 14:12:21 +0700

    remove path url strategy

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nota/app/bloc_observer.dart';
 import 'package:nota/features/notes/bloc/notes_bloc.dart';
 import 'package:nota/repositories/notes_repository.dart';
-import 'package:url_strategy/url_strategy.dart';
 
 import 'app/app.dart';
 import 'app/bloc/authentication_bloc.dart';
@@ -14,7 +13,6 @@ void main() async {
   Bloc.observer = AppBlocObserver();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  setPathUrlStrategy();
 
   final authenticationRepository = AuthenticationRepository();
 
